73e7b11fe565fb82a7fb961dac0d423f64641c63
bence <benceluzsinszky@gmail.com>
2024-10-22 09:59:14 +0200

    first working stream

--- client/client.go
+++ client/client.go
@@ -2,25 +2,76 @@ package main
 
 import (
 	proto "Chittychat/grpc"
+	"bufio"
 	"context"
+	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	// var opts []grpc.DialOption
+	// conn, err := grpc.NewClient(*serverAddr,
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect: %v", err)
+	// }
+	// defer conn.Close()
+
+	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatalf("Failed to connect: %v", err)
 	}
+	defer conn.Close()
 
 	client := proto.NewChittyChatClient(conn)
 
-	message, err := client.SendMessage(context.Background(), &proto.Message{ParticipantName: "Lucian", Message: "Hello from the outside", LamportTime: 123})
+	stream, err := client.Chat(context.Background())
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatalf("Failed to create stream: %v", err)
 	}
 
-	println(" - " + message.Message)
+	// Goroutine to send messages
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			if err := stream.Send(&proto.Message{Message: scanner.Text()}); err != nil {
+				log.Fatalf("Failed to send message: %v", err)
+			}
+		}
+		if err := stream.CloseSend(); err != nil {
+			log.Fatalf("Failed to close send stream: %v", err)
+		}
+	}()
+
+	// Receiving messages
+	for {
+		reply, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("Failed to receive message: %v", err)
+		}
+		fmt.Println("Received:", reply.Message)
+	}
 }
+
+// stream, err := client.RecordRoute(context.Background())
+// if err != nil {
+//   log.Fatalf("%v.RecordRoute(_) = _, %v", client, err)
+// }
+// for _, point := range points {
+//   if err := stream.Send(point); err != nil {
+//     log.Fatalf("%v.Send(%v) = %v", stream, point, err)
+//   }
+// }
+// reply, err := stream.CloseAndRecv()
+// if err != nil {
+//   log.Fatalf("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
+// }
+// log.Printf("Route summary: %v", reply)
--- server/server.go
+++ server/server.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	proto "Chittychat/grpc"
-	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -15,47 +15,35 @@ type ChittyChat struct {
 	messages []string
 }
 
-func (s *ChittyChat) SendMessage(ctx context.Context, in *proto.Message) (*proto.Message, error) {
-	fmt.Print(in)
-	s.messages = append(s.messages, in.Message)
-	return &proto.Message{Message: s.messages[0]}, nil
-}
-
-func (s *ChittyChat) Chat(ctx context.Context, in *proto.Message) (*proto.AllMessages, error) {
-	fmt.Print(in)
+func (s *ChittyChat) Chat(stream proto.ChittyChat_ChatServer) error {
 	for {
-		req, err := in
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		fmt.Println("Received:", in.Message)
 		s.messages = append(s.messages, in.Message)
+		for _, msg := range s.messages {
+			if err := stream.Send(&proto.Message{Message: msg}); err != nil {
+				return err
+			}
+		}
 	}
-	return &proto.Message{Message: s.messages[0]}, nil
 }
 
 func main() {
-	server := &ChittyChat{messages: []string{}}
-	message := proto.Message{
-		ParticipantName: "User123",
-		Message:         "Hello, ChittyChat!",
-		LamportTime:     1, // Example Lamport time (increment this based on your logic)
-	}
-
-	server.messages = append(server.messages, message.Message)
-
-	server.start_server()
-}
-
-func (s *ChittyChat) start_server() {
-	grpcServer := grpc.NewServer()
-	listener, err := net.Listen("tcp", ":5050")
+	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
-		log.Fatalf("Did not work")
+		log.Fatalf("failed to listen: %v", err)
 	}
 
-	proto.RegisterChittyChatServer(grpcServer, s)
+	s := grpc.NewServer()
+	proto.RegisterChittyChatServer(s, &ChittyChat{})
 
-	err = grpcServer.Serve(listener)
-
-	if err != nil {
-		log.Fatalf("Did not work")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
 	}
-
 }
