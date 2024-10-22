9c0e1143dde0957b24aa4ccaa2bf41a05b4e88e2
bence <benceluzsinszky@gmail.com>
2024-10-22 11:27:02 +0200

    working stream chat

--- server/server.go
+++ server/server.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync"
 
 	"google.golang.org/grpc"
 )
@@ -13,9 +14,39 @@ import (
 type ChittyChat struct {
 	proto.UnimplementedChittyChatServer
 	messages []string
+	clientId int
+	lamport  int64
+	clients  map[int]proto.ChittyChat_ChatServer
+	mu       sync.Mutex
+}
+
+func (s *ChittyChat) broadcast(message string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.lamport++
+	for _, client := range s.clients {
+		if err := client.Send(&proto.Message{Message: message, LamportTime: s.lamport}); err != nil {
+			log.Printf("Failed to send message to client: %v", err)
+		}
+	}
 }
 
 func (s *ChittyChat) Chat(stream proto.ChittyChat_ChatServer) error {
+	s.mu.Lock()
+	newClientId := s.clientId
+	s.clientId++
+	s.clients[newClientId] = stream
+	s.mu.Unlock()
+
+	s.broadcast(fmt.Sprintf("Participant %d joined Chitty-Chat at Lamport time %d", newClientId, s.lamport))
+
+	defer func() {
+		s.mu.Lock()
+		delete(s.clients, newClientId)
+		s.mu.Unlock()
+		s.broadcast(fmt.Sprintf("Participant %d left Chitty-Chat at Lamport time %d", newClientId, s.lamport))
+	}()
+
 	for {
 		in, err := stream.Recv()
 		if err == io.EOF {
@@ -24,14 +55,22 @@ func (s *ChittyChat) Chat(stream proto.ChittyChat_ChatServer) error {
 		if err != nil {
 			return err
 		}
-		fmt.Println("Received:", in.Message)
-		s.messages = append(s.messages, in.Message)
-		for _, msg := range s.messages {
-			if err := stream.Send(&proto.Message{Message: msg}); err != nil {
-				return err
-			}
-		}
+
+		s.mu.Lock()
+		s.lamport = max(s.lamport, in.LamportTime) + 1
+		savedMessage := fmt.Sprintf("Client %d: %s at Lamport Time %d", newClientId, in.Message, s.lamport)
+		s.messages = append(s.messages, savedMessage)
+		s.mu.Unlock()
+
+		// bigMessage := ""
+		// for _, msg := range s.messages {
+		// 	bigMessage += msg + "\n"
+		// }
+
+		// s.broadcast(bigMessage)
+		s.broadcast(savedMessage)
 	}
+
 }
 
 func main() {
@@ -39,9 +78,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
+	log.Println("Server listening on port 8080")
 
 	s := grpc.NewServer()
-	proto.RegisterChittyChatServer(s, &ChittyChat{})
+	proto.RegisterChittyChatServer(s, &ChittyChat{
+		clients: make(map[int]proto.ChittyChat_ChatServer),
+	})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
